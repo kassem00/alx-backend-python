c4194210f981d33f3b0788ed70343e9999718a1f
kassem00 <kamsdonga@gmail.com>
2024-08-02 06:02:41 +0000

    au

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ sum_list  """
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ sum_list """
     x: float = 0
     for value in input_list:
