f31e91c131df563eac4a40cbf05ddc2aca1ab396
kassem00 <kamsdonga@gmail.com>
2024-08-02 04:39:34 +0000

    au

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" add two nums """
+
+
+def to_str(n: float) -> str:
+    """
+    type-annotated function add that takes a float a and a float
+    b as arguments and returns
+    """
+    return str(n)
