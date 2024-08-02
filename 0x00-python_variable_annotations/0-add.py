b757aa709cd1a8366b1320e3b67e6f877fd486f0
kassem00 <kamsdonga@gmail.com>
2024-08-02 01:56:49 +0000

    au

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def add(a: float, b: float) -> float:
+    """
+    type-annotated function add that takes a float a and a float
+    b as arguments and returns
+    """
+    return a + b
