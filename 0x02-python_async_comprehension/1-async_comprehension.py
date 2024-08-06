48723ba90ab0a33b098b31fe3c9643079f8a87e8
kassem00 <kamsdonga@gmail.com>
2024-08-06 16:21:17 +0000

    au

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module containing the async_generator coroutine."""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> AsyncGenerator[float, None]:
+    """
+    Coroutine that loops 10 times, asynchronously waits 1 second each time,
+    and yields a random number between 0 and 10.
+    """
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result
