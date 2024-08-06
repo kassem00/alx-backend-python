ae1e727b97fd55ca7bc044cf66ff55685cc6264c
kassem00 <kamsdonga@gmail.com>
2024-08-06 15:09:12 +0000

    au

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-""" async_generator """
+"""Module containing the async_generator coroutine."""
+
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator() -> int:
-    """ coroutine called async_generator that takes no arguments """
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Coroutine that loops 10 times, asynchronously waits 1 second each time,
+    and yields a random number between 0 and 10.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
