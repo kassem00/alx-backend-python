d68f30910002a80be60c747e7a3936445117c8e2
kassem00 <kamsdonga@gmail.com>
2024-08-06 15:06:04 +0000

    au

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> int:
     """ coroutine called async_generator that takes no arguments """
     for _ in range(10):
         await asyncio.sleep(1)
