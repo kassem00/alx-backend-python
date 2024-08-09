99b18c7a855f86b2217b25dde0d610e25006672a
kassem00 <kamsdonga@gmail.com>
2024-08-09 19:25:23 +0000

    au

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -11,7 +11,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Create a list of tasks using task_wait_random 
+    Create a list of tasks using task_wait_random
     and return their completion times sorted.
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
