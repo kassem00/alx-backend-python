b6fe9d8f203d5c6c4706510c1078d3b8ba650565
kassem00 <kamsdonga@gmail.com>
2024-08-09 19:23:46 +0000

    au

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module to create an asyncio Task that schedules multiple coroutines."""
+
+
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Create a list of tasks using task_wait_random 
+    and return their completion times sorted.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
