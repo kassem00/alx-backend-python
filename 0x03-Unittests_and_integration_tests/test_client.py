7e6c7882e29c224d092e0568a030a965aab86f34
kassem00 <kamsdonga@gmail.com>
2024-08-27 02:53:05 +0000

    au

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+unit test for utils.access_nested_map.
+"""
+from unittest import unittest.TestCase
+from utils import access_nested_map
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ unit test for utils.access_nested_map. """
+
+    def test_access_nested_map():
+        """ test_access_nested_map """
