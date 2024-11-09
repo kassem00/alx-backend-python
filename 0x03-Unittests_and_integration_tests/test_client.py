a52bd8f4830367b7c994403e82dd338c03904a31
kassem00 <kamsdonga@gmail.com>
2024-11-09 10:45:22 +0000

    au

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -9,13 +9,19 @@ from utils import access_nested_map
 class TestGithubOrgClient(unittest.TestCase):
     """ unit test for utils.access_nested_map. """
 
-
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": {"c": 3}}}, ("a", "b", "c"), 3),
+        ({"a": {"b": {"c": 3}}}, ("a", "b", "d"), None),
+    ])
     def test_access_nested_map(self, nested_map, path, expected):
         """ test_access_nested_map """
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+        s
     def test_public_repos(self):
         """ test_access_nested_map """
-        
+        pass
 
 if __name__ == "__main__":
     unittest.main()
