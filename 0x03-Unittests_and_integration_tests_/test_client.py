d7b3843fc337a16ba950690e0e774f3f36c6fa64
kassem00 <kamsdonga@gmail.com>
2024-11-09 13:14:03 +0000

    au

--- 0x03-Unittests_and_integration_tests_/test_client.py
+++ 0x03-Unittests_and_integration_tests_/test_client.py
@@ -2,25 +2,30 @@
 """
 unit test for utils.access_nested_map.
 """
-
-
-from unittest import unittest.TestCase
+import unittest
 from utils import access_nested_map
+from unittest import TestCase
+from test_client import access_nested_map 
 
 
-class TestAccessNestedMap(unittest.TestCase, nested_map, path, expected):
+class TestGithubOrgClient(unittest.TestCase):
     """ unit test for utils.access_nested_map. """
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": {"c": 3}}}, ("a", "b", "c"), 3),
+        ({"a": {"b": {"c": 3}}}, ("a", "b", "d"), None),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """ Test that the method returns what it is supposed to """
+        """Test access_nested_map with valid paths."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    def test_public_repos(self):
+        """ test_access_nested_map """
+        pass
 
 
 if __name__ == "__main__":
-    """ main test file"""
     unittest.main()
--- 0x03-Unittests_and_integration_tests_/test_utils.py
+++ 0x03-Unittests_and_integration_tests_/test_utils.py
@@ -2,25 +2,21 @@
 """
 unit test for utils.access_nested_map.
 """
-
-
 from unittest import unittest.TestCase
 from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase, nested_map, path, expected):
     """ unit test for utils.access_nested_map. """
-
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        nested_map={"a": 1}, path=("a",)
+        nested_map={"a": {"b": 2}}, path=("a",)
+        nested_map={"a": {"b": 2}}, path=("a", "b")
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """ Test that the method returns what it is supposed to """
+        """ test_access_nested_map """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
 
 if __name__ == "__main__":
-    """ main test file"""
     unittest.main()
